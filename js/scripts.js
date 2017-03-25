0748afe61f35b03535068c302ac3e9dd2fc62b86
JanWrotkowski <trojmiasto2@wp.pl>
2017-03-25 15:13:32 +0100

    ...

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// plik scripts.js
+
+var name = prompt('im java script idiot, and i want destroy all you work so just click ok if u agree');
+alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
